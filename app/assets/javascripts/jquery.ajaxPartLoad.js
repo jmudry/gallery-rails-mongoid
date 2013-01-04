37694fc58d123ba52c8abec631d045791d1b4d30
Jan Mudry <jairek@o2.pl>
2013-01-04 08:43:39 +0100

    jshint

--- app/assets/javascripts/jquery.ajaxPartLoad.js
+++ app/assets/javascripts/jquery.ajaxPartLoad.js
@@ -1,34 +1,34 @@
-(function($) {
-    $.fn.ajaxPartLoad = function(options) {
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, undef:true, unused:true, curly:true, browser:true, jquery:true, indent:4, maxerr:50 */
+(function ($) {
+    $.fn.ajaxPartLoad = function (options) {
         var settings;
-        var onclick = function() {
-            $(this).attr('data-disabled',true).addClass("ajax-load").find(".middle").text("");
+        var onclick = function () {
+            $(this).attr('data-disabled', true).addClass("ajax-load").find(".middle").text("");
             var that = $(this);
             $.ajax({
                 type: 'POST',
                 url: $(this).attr('href'),
-                success: function(data) {
-                    var load_to = that.data('load-to');
-                    $(settings.loadTo+"").append(data.html);
+                success: function (data) {
+                    $(settings.loadTo + "").append(data.html);
                     if (data.url === "end") {
                         that.hide();
                     } else {
-                        that.removeAttr('data-disabled').attr('href',data.url).removeClass("ajax-load").find('.middle').text('Load more');
+                        that.removeAttr('data-disabled').attr('href', data.url).removeClass("ajax-load").find('.middle').text('Load more');
                     }
                 }
             });
             return false;
         };
 
-        return this.each(function() {
+        return this.each(function () {
             var that = $(this);
-            settings = $.extend( {
+            settings = $.extend({
                 visibility : 500,
                 loadTo: typeof that.data('load-to') !== 'undefined' ? that.data('load-to') : 'body'
             }, options);
             that.live('click', onclick);
-            $(window).scroll(function() {
-                if($(window).scrollTop() + $(window).height() > $(document).height() - settings.visibility) {
+            $(window).scroll(function () {
+                if ($(window).scrollTop() + $(window).height() > $(document).height() - settings.visibility) {
                     if (that.is(":visible") && that.data("disabled") !== false) {
                         $('.show-more-ajax:visible:not([data-disabled])').trigger('click');
                     }
